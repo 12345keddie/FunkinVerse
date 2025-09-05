d99d56e9c539abc32ecf4b14fee5756676ae8b2d
Leo Keddie <leonardokeddie@gmail.com>
2025-09-05 06:13:27 +0000

    Look up mod banners on GameBanana and use them instead of stock images

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.gamebanana.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
