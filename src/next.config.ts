5e27d4f05b57ba81f7178a401e46a2b4ef4c18e7
Leo Keddie <leonardokeddie@gmail.com>
2025-09-18 06:01:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/next.config.ts
@@ -0,0 +1,53 @@
+import type {NextConfig} from 'next';
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https' ,
+        hostname: 'images.gamebanana.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.wikia.nocookie.net',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  // Allow cross-origin requests in development
+  ...(process.env.NODE_ENV === 'development' && {
+    experimental: {
+      allowedDevOrigins: ['https://*.cloudworkstations.dev'],
+    },
+  }),
+};
+
+export default withPWA(nextConfig);
