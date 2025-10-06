8b3bb676fab43d1a384ee7813f0433d65ac1507b
Leo Keddie <leonardokeddie@gmail.com>
2025-10-06 16:19:34 +0000

    Remove the click anywhere text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,11 +57,6 @@ export default function VideoPage() {
       >
         Your browser does not support the video tag.
       </video>
-      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white" style={{pointerEvents: 'none'}}>
-        <div className="text-center">
-            <h1 className="text-2xl font-bold">Click anywhere or press any key to play</h1>
-        </div>
-      </div>
     </div>
   );
 }
